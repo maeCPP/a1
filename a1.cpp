6c1d19e4d7820a0a87a07fe883f1408a5827ebe3
Mae Yee <mae.cast@hotmail.com>
2015-11-07 22:08:58 -0800

    removed comments.  passing most tests.  not complete.

--- a1.cpp
+++ a1.cpp
@@ -12,6 +12,7 @@
 #include <map>
 #include <regex>
 #include <cstdlib>
+#define DEFAULT_COLOR "\033[39;49m"
 
 using namespace std;
 
@@ -111,16 +112,6 @@ map<string, string> load_config(int argc, char* argv[])
         exit(1);
     }
 
-/*  
-    !!! TESTING MAP -- REMOVE CODE BEFORE SUBMITTING
-
-    cout << "------------------------------------------------------" << endl;
-    for (auto& i: config_map) {
-        cout << i.first << "\t=> " << i.second << endl;
-    }
-    cout << "------------------------------------------------------" << endl;
-
-*/
     #ifdef DEBUG
         for (auto x: config_map)
             cout << x.second << "<" << x.first << ">" << it->second << endl;
@@ -136,54 +127,39 @@ int main(int argc, char* argv[])
     string line, word;
     map<string, string> config_map;
     vector<string> v;
+
     smatch m;
     string prefix, suffix;
-
     regex reg_tag(R"(<(.+?)>)");
     regex reg_ot(R"(<text>)");
     regex reg_ct(R"(</text>)");
+
     bool ot_flag = false;               // true if open text tag  <text>  found
     bool ct_flag = false;               // true if close text tag </text> found
     size_t lineNum = 0;                 // line number
-    size_t tagNum = 0;                  // tag number
 
     // load configuration file
     config_map = load_config(argc, argv);
 
-    //cout << "152:  (ot_flag, ct_flag):  (" << ot_flag << ',' << ct_flag << ')' << endl;
     // get first word and check that it is the <text> tag
     cin >> word;
 
-    if (word.substr(0,6) != "<text>") {
+    if (word.substr(0,6) != "<text>") 
+    {
         cerr << "\n\nERROR:  Input text does not start with <text> tag.  Exiting program." << endl;
          exit(1);
     } else {
         v.push_back("text");
-        tagNum++;
         ot_flag = true;
         word = word.substr(6);
         auto it = config_map.find("text");
         cout << it->second;                   
-        // cout << "\nnew word: " << word << endl;
     }
 
-
-    /* TO-DO:  handle case where input text does not end with </text> tag */
-
-    /* TO-DO:  handle case where <text> or </text> occur more than once */
-
-    //cout << "173:  (ot_flag, ct_flag):  (" << ot_flag << ',' << ct_flag << ')' << endl;
     // process all the rest
     while (getline(cin, line)) 
     {
         lineNum++;
-        //cout << "line: " << lineNum << "    ";
-        
-        // preserve lines with only white space
-        //if (is_white_space(line)) {
-        //    // cout << "line number: " << lineNum << endl;
-        //    cout << endl;
-        //}
 
         if (lineNum == 1)
             line = word + line;
@@ -195,54 +171,16 @@ int main(int argc, char* argv[])
             string tagname = m.str(1);
 
             cout << prefix;
-
-            //cout << "\n167:  <" << tagname << ">  lineNum:  " << lineNum << "  ot_flag? " << ot_flag << ' ' << "tagNum: " << tagNum << endl; 
-
-            // // in first line
-            // if (lineNum == 1) 
-            // {
-            //     // in first line, found <text> and no previous tags have been scanned in
-            //     if (tagname == "text" && tagNum == 0)
-            //     {
-            //         tagNum++;
-            //         ot_flag = true;
-
-
-            //         // check that <text> is preceeded only by white space
-            //         if (!is_white_space(m.prefix())) 
-            //         {
-            //             cerr << "\033[39;49m\n\nERROR:  182  Input text does not start with <text> tag.  Exiting program." << endl;
-            //             exit(1);
-            //         }
-
-            //         // check that <text> tag not duplicated
-            //         //if (ot_flag) {
-            //         //    cerr << "\033[39;49m\n\nERROR:  duplicate <text> tag on line " << lineNum << ".  Exiting program." << endl;
-            //         //    exit(1);
-            //         //}
-                    
-
-            //         //cout << "191:  " << tagname << "  ot_flag? " << ot_flag << ' ' << "tagNum: " << tagNum << endl; 
-
-            //         // check that the <text> tag is not preceded by non white space characters
-                    
-            //     // in first line, found <text> but it's not the first tag in that line
-            //     } else if (tagname == "text" && tagNum != 0) {
-            //         cerr << "\033[39;49m\n\nERROR:  198  Input text does not start with <text> tag.  Exiting program." << endl;
-            //         exit(1);
-            //     // tag is anything but <text>
-            //     } else {
             
             if (tagname[0] != '/')                      // it is a start tag 
             {
                 // check that there is no duplication of <text> tag
                 if (tagname == "text" && ot_flag)
                 {  
-                    cerr << "\033[39;49m\n\nERROR:  duplicate <text> tag.  Exiting program." << endl;
+                    cerr << DEFAULT_COLOR << "\n\nERROR:  duplicate <text> tag.  Exiting program." << endl;
                     exit(1);
                 }
 
-                tagNum++;
                 auto it = config_map.find(tagname);     // lookup tag in config map
 
                 if (it != config_map.end())             // tag is found
@@ -250,59 +188,46 @@ int main(int argc, char* argv[])
                     cout << it->second;
                     v.push_back(tagname);    
                 } else {
-                    cerr << "\033[39;49m\n\nERROR:  <" << tagname << "> is an invalid tag.  Exiting program." << endl;
+                    cerr << DEFAULT_COLOR << "\n\nERROR:  <" << tagname << "> is an invalid tag.  Exiting program." << endl;
                     exit(1);
                 }
-            //cout << "254:  (ot_flag, ct_flag):  (" << ot_flag << ',' << ct_flag << ')' << endl;
+
             } else {                                    // it is an end tag
 
-                if (tagname == "/text") {
-                    //cout << "-------- found /text" << endl;
+                if (tagname == "/text") 
+                {
                     ct_flag = true;
                 }
                 
-
-                // if (!v.empty()) {
-
-                    /*
-                    cerr << "v = [";
-                    for (auto vit = v.begin(); vit != v.end()-1; ++vit)
-                        cerr << *vit << ", ";
-                    cerr << v.back() << "]" << endl;
-                    */
-                   
-
+                // check if tagname is the last element of the vector
+                if (v.back() == tagname.substr(1))
+                {
+                    // remove the last tagname from the vector
+                    v.pop_back();
                     
-                    // check if tagname is the last element of the vector
-                    if (v.back() == tagname.substr(1))
+                    // revert to previous color   
+                    if (!v.empty())
                     {
-                        // remove the last tagname from the vector
-                        v.pop_back();
-                        
-                        // revert to previous color   
-                        if (!v.empty())
-                        {
-                            auto it = config_map.find(v.back());
-                            cout << it->second;                            
-                        }
+                        auto it = config_map.find(v.back());
+                        cout << it->second;                            
+                    }
 
-                    } else {                            // invalid nesting of tags
-                        cerr << "\033[39;49m\n\nERROR:  Invalid nesting of tags.  Exiting program." << endl;
-                        exit(1);
-                    } 
-                // }
-                //cout << "292:  (ot_flag, ct_flag):  (" << ot_flag << ',' << ct_flag << ')' << endl;
+                } else {                                // invalid nesting of tags
+                    cerr << DEFAULT_COLOR << "\n\nERROR:  Invalid nesting of tags.  Exiting program." << endl;
+                    exit(1);
+                } 
             } 
             
-        } // closes for loop
+        } //END: for loop
 
-        //cout << "this is the last one:  " << suffix << endl;
-        if (!is_white_space(line)) 
+
+        if (!is_white_space(line) && !ct_flag) 
+            //cout << " ?FLAG? " << ct_flag << " length: " << suffix.length() << ": " << suffix << endl;
             cout << suffix << endl;
-        else
-            cout << endl;
-
-        //cout << "300: (ot_flag, ct_flag):  (" << ot_flag << ',' << ct_flag << ')' << endl;
+        
+        //if (is_white_space(line) && !ct_flag) {
+        //    cout << endl;
+        //}
 
         if (!regex_search(line, m, reg_tag))
         {
@@ -311,15 +236,16 @@ int main(int argc, char* argv[])
 
             if (ct_flag && !is_white_space(line)) 
             {
-                cerr << "\033[39;49m\n\nERROR:  Text found after </text> tag.  Exiting program." << endl;
+                cerr << DEFAULT_COLOR << "\n\nERROR:  Text found after </text> tag.  Exiting program." << endl;
                 exit(1);
             }
         } 
 
-    } // closes while loop
+    } // END: while loop
 
+    // check if </text> tag found after reading in all input text
     if (!ct_flag) {
-        cerr << "\033[39;49m\n\nERROR:  No </text> tag found.  Exiting program." << endl;
+        cerr << DEFAULT_COLOR << "ERROR:  No </text> tag found.  Exiting program." << endl;
         exit(1);
     }
 
